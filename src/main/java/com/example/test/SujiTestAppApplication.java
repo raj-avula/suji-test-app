dab9c12a9bcefad52e1c0d6bc036e150cb2641e3
Raj Avula <avula810@gmail.com>
2021-06-28 18:00:34 -0500

    Initial commit

---
+++ src/main/java/com/example/test/SujiTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SujiTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SujiTestAppApplication.class, args);
+	}
+
+}
